4d8153f8063f5b68f79f6bb2969057610cfd2449
DavidVed <davidved10@gmail.com>
2023-09-30 13:54:05 +0300

    Third commit

--- src/main/java/org/example/FirstSeleniumScript.java
+++ src/main/java/org/example/FirstSeleniumScript.java
@@ -4,7 +4,6 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-import static java.lang.Thread.*;
 
 public class FirstSeleniumScript {
     public static void main(String[] args) throws InterruptedException {
